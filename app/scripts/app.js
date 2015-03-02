b49d62ba820a617eec9a3be3e5547ba9134507c1
Keisuke Noguchi <windy.imlovinit@gmail.com>
2015-03-02 21:56:10 +0900

    ルーティングの無駄な記述を削除

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,10 +28,6 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
